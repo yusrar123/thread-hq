3760b51eb9521f170319685bba9534ce92f8a3c4
yusrar123 <yusrar@thread-hq.com>
2025-06-19 21:47:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Welcome to Thread HQ!");
+const text = "Review brands. Expose scams. Empower creators.";
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typing-text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 50);
+  }
+}
+
+window.onload = typeWriter;
