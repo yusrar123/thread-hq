31b1cda5f65124b7b28d3868b12a52a719ae6064
yusrar123 <yusrar@thread-hq.com>
2025-07-01 16:46:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-// Get form element
 const signupForm = document.getElementById("signupForm");
 
-// Listen for form submit
 signupForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  // Get form values
   const name = signupForm.elements[0].value;
   const email = signupForm.elements[1].value;
   const password = signupForm.elements[2].value;
 
-  // Sign up user with Firebase Auth
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-
-      // Optional: Save name to Firebase user profile
       return user.updateProfile({
         displayName: name
       });
