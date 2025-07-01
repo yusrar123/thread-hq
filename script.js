e2dc33f221f57b89648608d3e6ef0757dbc73bc2
yusrar123 <yusrar@thread-hq.com>
2025-07-01 20:15:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Import Firebase functions from the SDK
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+// Initialize Firebase Auth
+const auth = getAuth();
+
+// Get form element
 const signupForm = document.getElementById("signupForm");
 
 signupForm.addEventListener("submit", function (e) {
@@ -7,10 +14,13 @@ signupForm.addEventListener("submit", function (e) {
   const email = signupForm.elements[1].value;
   const password = signupForm.elements[2].value;
 
-  firebase.auth().createUserWithEmailAndPassword(email, password)
+  // Sign up user
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-      return user.updateProfile({
+
+      // Update the user's display name
+      return updateProfile(user, {
         displayName: name
       });
     })
