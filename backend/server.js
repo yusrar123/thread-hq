f310d05f2a87714198e2932f04d9c62b4ead7af3
sub-aina <subainamonib3@gmail.com>
2025-07-24 13:45:51 +0500

    Trigger redeploy to fix CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
         'http://localhost:3000',
         'http://localhost:5173',
         'http://localhost:4173',
-        'https://thread-inky.vercel.app/',
+        'https://thread-inky.vercel.app',
         // 'https://thread-hq-sub-ainas-projects.vercel.app', // this is your deployed frontend
     ],
     credentials: true,
