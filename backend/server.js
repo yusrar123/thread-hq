9476f310e728917a79917db494db22c68aa340e9
sub-aina <subainamonib3@gmail.com>
2025-07-25 20:06:05 +0500

    render url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
         'http://localhost:3000',
         'http://localhost:5173',
         'http://localhost:4173',
-        'https://thread-inky.vercel.app',
+        // 'https://thread-inky.vercel.app',
         'https://www.thread-hq.com'
 
     ],
