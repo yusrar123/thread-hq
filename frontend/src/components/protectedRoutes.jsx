6c27fc4d0fbada349b57edb59b026bdd5c5cacc0
sub-aina <subainamonib3@gmail.com>
2025-07-25 15:38:34 +0500

    console logs removed

--- frontend/src/components/protectedRoutes.jsx
+++ frontend/src/components/protectedRoutes.jsx
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
 	const token = localStorage.getItem("token");
-	console.log("Token in ProtectedRoute:", token);
 
 	return token ? children : <Navigate to="/login" />;
 }
